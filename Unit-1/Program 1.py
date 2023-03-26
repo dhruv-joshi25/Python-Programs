b5909551fab6ad5444025dd86c6e4d44a243f315
dhruvj2504 <92200593+dhruvj2504@users.noreply.github.com>
2023-03-26 20:08:48 +0530

    Create Program 1.py

---
+++ Unit-1/Program 1.py
@@ -0,0 +1,18 @@
+#Write a simple Python Program to INPUT two variables
+#and print Addition, Subtraction, Multiplication and Division of both numbers.
+
+var1=int(input("Enter Any Number : "))
+var2=int(input("Enter Any Number : "))
+
+def sum():
+    return print("Addition Is       : ",var1+var2)
+def sub():
+    return print("Substraction Is   : ",var1-var2)
+def mul():
+    return print("Multiplication Is : ",var1*var2)
+def div():
+    return print("Division Is       : ",var1/var2)
+sum()
+sub()
+mul()
+div()
