9f759b4a7f6a52eb9a1cfcd01bf078d10aeca864
Dhruv Joshi <92200593+dhruvj2504@users.noreply.github.com>
2023-03-27 19:06:09 +0530

    Add files via upload

--- Unit-4/Program 31.py
+++ Unit-4/Program 31.py
@@ -1,1 +1,21 @@
+(""""Write a Python Program that creates a Student class with various methods.
+Use setattr() and getattr() on class object.""")
 
+class student:
+    name = "Dhruv"
+    roll_no = 101
+    address = "Gondal"
+
+std = student()
+
+setattr(std, "name" , "Dhruv")
+a = getattr(std, "name")
+print(a)
+
+setattr(std , "roll_no", 101)
+b = getattr(std, "roll_no")
+print(b)
+
+setattr(std , "address", "Gondal")
+b = getattr(std, "address")
+print(b)
---
+++ Unit-4/Program 32.py
@@ -0,0 +1,5 @@
+("""Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.""")
+
+
